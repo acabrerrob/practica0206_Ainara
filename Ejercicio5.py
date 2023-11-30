bf3604ce7557ae18096520debffd552005a648f7
acabrerrob <acabrerrob@educacion.navarra.es>
2023-11-30 16:33:04 +0100

    enunciados

---
+++ Ejercicio5.py
@@ -0,0 +1,4 @@
+# Escribir un programa que abra el fichero con información sobre
+# el PIB per cápita de los países de la Unión Europea (), pregunte
+# por las iniciales de un país y muestre el PIB per cápita de ese 
+# país de todos los años disponibles.
