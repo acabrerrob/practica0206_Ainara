56db928de20f11ff38251517d17245f6e5da2156
acabrerrob <acabrerrob@educacion.navarra.es>
2023-12-11 20:26:35 +0100

    Update Ejercicio3.py

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -2,3 +2,12 @@
 # lea el fichero tabla-n.txt con la tabla de multiplicar de ese
 # número, y muestre por pantalla la línea m del fichero. Si el
 # fichero no existe debe mostrar un mensaje por pantalla informando de ello.
+def lineaFichero ():
+
+    n = int(input('Introduzca un número entre 1 y 10: '))
+    m = int(input('Introduzca un número entre 1 y 10: '))
+
+    file = open(f'tabla-{n}.txt', 'r')
+    print(file.read(m))
+    return
+lineaFichero()
