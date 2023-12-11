a516cb5263f00ee47c3d6e9ab10d61fba287d783
acabrerrob <acabrerrob@educacion.navarra.es>
2023-12-11 20:07:07 +0100

    .

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,12 +1,12 @@
 # Escribir una función que pida un número entero entre 1 y 10 y
 # guarde en un fichero con el nombre tabla-n.txt la tabla de 
 # multiplicar de ese número, donde n es el número introducido.
-def newFicheroMult (number):
-
+def newFicheroMult ():
+    number = int(input('introduzca un numero'))
     file = open(f'tabla-{number}.txt','w')
-    for n in range (1,11):
+    for n in range (0,11):
         value = n * number
         file.write(f'\n{n} * {number} = {value}')
-    return value
+    return 
 
-newFicheroMult(4)
+newFicheroMult()
