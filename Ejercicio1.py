a773a20b597eaec31400a195e1e06ac69440c899
acabrerrob <acabrerrob@educacion.navarra.es>
2023-12-11 20:17:37 +0100

    .

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -2,11 +2,15 @@
 # guarde en un fichero con el nombre tabla-n.txt la tabla de 
 # multiplicar de ese número, donde n es el número introducido.
 def newFicheroMult ():
+
     number = int(input('introduzca un numero'))
+
     file = open(f'tabla-{number}.txt','w')
+
     for n in range (0,11):
         value = n * number
         file.write(f'\n{n} * {number} = {value}')
+        
     return 
 
 newFicheroMult()
--- Ejercicio2.py
+++ Ejercicio2.py
@@ -2,3 +2,14 @@
 # lea el fichero tabla-n.txt con la tabla de multiplicar de ese
 # número, donde n es el número introducido, y la muestre por pantalla.
 # Si el fichero no existe debe mostrar un mensaje por pantalla informando de ello.
+def ficheroPrinter ():
+
+    number = int(input('Introduzca un número entre 1 y 10: '))
+    
+    file = open(f'tabla-{number}.txt', 'r')
+    
+    print(file.read())
+
+    return
+
+ficheroPrinter()
